457b49efe1f6004e98581fe481be06a87d403495
antuone <asikuo@gmail.com>
2018-09-16 12:47:22 +0500

    Update cab.autoinline.com.js

--- Sites/cab.autoinline.com.js
+++ Sites/cab.autoinline.com.js
@@ -1,3 +1,3 @@
 
-document.querySelector('#login-form > form > div:nth-child(2) > input').value = 'asikuo@gmail.com';
-document.querySelector('#login-form > form > div:nth-child(3) > input').value = 'ccm9TT=7';
+document.querySelector('#login-form > form > div:nth-child(2) > input').value = '';
+document.querySelector('#login-form > form > div:nth-child(3) > input').value = '';
